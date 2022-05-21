b65152e6b76e5a25ae1154a971c26e7bd72ce104
Dave Mbassi <dcmbassi@gmail.com>
2022-05-21 15:46:36 +0100

    feat: Implement REST API logic for Member model

---
+++ pages/api/members/index.js
@@ -0,0 +1,33 @@
+import Member from '../../../models/memberModel'
+import dbConnect from '../../../db/connect'
+
+const handler = async (req, res) => {
+    const {method} = req
+
+    await dbConnect()
+
+    switch (method) {
+        case 'GET':
+            try {
+                const members = await Member.find()
+                res.status(200).json({success: true, data: members})
+            } catch (error) {
+                res.status(400).json({success: false, message: error.message})
+            }
+            break;
+        case 'POST':
+            try {
+                const member = await Member.create(req.body)
+                res.status(201).json({success: true, data: member})
+            } catch (error) {
+                res.status(400).json({success: false, message: error.response.data})
+            }
+            break;
+    
+        default:
+            res.status(400).json({success: false})
+            break;
+    }
+}
+
+export default handler
