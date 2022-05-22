65a18ba38e4b4153576c8196c681a1b5707e88ba
Dave Mbassi <dcmbassi@gmail.com>
2022-05-22 19:07:09 +0100

    feat: Create reusable Head component for SEO

---
+++ layouts/Meta.jsx
@@ -0,0 +1,13 @@
+import Head from "next/head"
+
+const Meta = ({title, description}) => {
+    return (
+        <Head>
+            <title>Yigli {title && `| ${title}`}</title>
+            <meta name="description" content={description} />
+            {/* <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" /> */}
+        </Head>
+    )
+}
+
+export default Meta
