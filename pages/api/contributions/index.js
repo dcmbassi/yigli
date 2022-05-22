f7f34ec1d5dcbbb6cfd098e1e17d7926ea13815a
Dave Mbassi <dcmbassi@gmail.com>
2022-05-22 14:58:41 +0100

    feat: Implement REST API logic for contributions model

---
+++ pages/api/contributions/index.js
@@ -0,0 +1,33 @@
+import Contribution from '../../../models/contributionModel'
+import dbConnect from '../../../db/connect'
+
+const handler = async (req, res) => {
+    const {method} = req
+
+    await dbConnect()
+
+    switch (method) {
+        case 'GET':
+            try {
+                const contributions = await Contribution.find()
+                res.status(200).json({success: true, data: contributions})
+            } catch (error) {
+                res.status(400).json({success: false, message: error.message})
+            }
+            break;
+        case 'POST':
+            try {
+                const contribution = await Contribution.create(req.body)
+                res.status(201).json({success: true, data: contribution})
+            } catch (error) {
+                res.status(400).json({success: false, message: error.response.data})
+            }
+            break;
+    
+        default:
+            res.status(400).json({success: false})
+            break;
+    }
+}
+
+export default handler
