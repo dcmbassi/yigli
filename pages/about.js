769bc90584a5e30f9bd9634e4224bf615cc81488
Dave Mbassi <dcmbassi@gmail.com>
2022-05-23 16:00:24 +0100

    feat: Set up MUI architecture

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,20 @@
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
 const AboutPage = () => {
   return (
-    <div>About Page</div>
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Déscendance de Jean-Baptiste Medoung, dit Yigli
+        </Typography>
+        <Button variant="contained"  noLinkStyle href="/">
+          Go to the main page
+        </Button>
+      </Box>
+    </Container>
   )
 }
 
