40dbd2f841e375a1b5a3b724fd082545a31f8e9f
Dave Mbassi <dcmbassi@gmail.com>
2022-06-13 17:54:20 +0100

    chore: Adjust input formatting in Modular form

--- components/ModularForm.jsx
+++ components/ModularForm.jsx
@@ -5,7 +5,7 @@ const ModularForm = ({ data }) => {
     const [formValues, setFormValues] = useState(data.initialValues)
     const [successMessage, setSuccessMessage] = useState('')
     const [success, setSuccess] = useState(false)
-    const {title} = data
+    const { title } = data
 
     useEffect(() => {
         if (success) setSuccessMessage(`${title} created`)
@@ -17,7 +17,7 @@ const ModularForm = ({ data }) => {
 
     const handleInputChange = e => {
         const value = e.target.value
-        setFormValues({...formValues, [e.target.name]: value})
+        setFormValues({ ...formValues, [e.target.name]: value })
     }
 
     const resetFields = () => {
@@ -31,14 +31,14 @@ const ModularForm = ({ data }) => {
             TO DO:
             1. Validate inputs
         */
-        
+
         try {
             const result = await submitForm(data.endpoint, formValues)
             console.log(result)
             if (result.success) {
                 setSuccess(true)
                 resetFields()
-                
+
             }
         } catch (error) {
             console.log(error)
@@ -47,35 +47,48 @@ const ModularForm = ({ data }) => {
 
     return (
         <>
-        <form onSubmit={handleSubmit}>
-            {data.fields.map(item => (
-                <div key={item.name}>
-                    {item.type === 'radio'
-                        ? (
-                            <fieldset>
-                                <legend>{item.displayedText}</legend>
-                                {item.options.map(option => (
-                                    <p key={option.key}>
-                                        <input type={item.type} name={item.name} id={option.value} checked={formValues[item.name] === option.value} value={option.value} onChange={handleInputChange} />
-                                        <label htmlFor={option.value}>{option.key}</label>
-                                    </p>
-                                ))}
-                            </fieldset>
-                        )
-                        : (
-                            <>
-                                <label htmlFor={item.name}>{item.displayedText}: </label>
-                                <input type={item.type} name={item.name} id={item.name} value={formValues[item.name]} onChange={handleInputChange} />
-                            </>
-                        )
-                    }
-                    <br />
-                    <br />
-                </div>
-            ))}
-            <input type="submit" value="Enregistrer" />
-        </form>
-        <p>{successMessage}</p>
+            <form onSubmit={handleSubmit}>
+                {data.fields.map(item => (
+                    <div key={item.name}>
+                        {item.type === 'radio'
+                            ? (
+                                <fieldset>
+                                    <legend>{item.displayedText}</legend>
+                                    {item.options.map(option => (
+                                        <p key={option.key}>
+                                            <input
+                                                type={item.type}
+                                                name={item.name}
+                                                id={option.value}
+                                                checked={formValues[item.name] === option.value}
+                                                value={option.value}
+                                                onChange={handleInputChange}
+                                            />
+                                            <label htmlFor={option.value}>{option.key}</label>
+                                        </p>
+                                    ))}
+                                </fieldset>
+                            )
+                            : (
+                                <>
+                                    <label htmlFor={item.name}>{item.displayedText}: </label>
+                                    <input
+                                        type={item.type}
+                                        name={item.name}
+                                        id={item.name}
+                                        value={formValues[item.name]}
+                                        onChange={handleInputChange}
+                                    />
+                                </>
+                            )
+                        }
+                        <br />
+                        <br />
+                    </div>
+                ))}
+                <input type="submit" value="Enregistrer" />
+            </form>
+            <p>{successMessage}</p>
         </>
     )
 }
