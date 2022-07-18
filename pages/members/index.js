5cdc18050d276a6c5414feb8133b47561e071fcf
Dave Mbassi <dcmbassi@gmail.com>
2022-07-18 20:29:13 +0100

    fix: Fetch members on Members index page

--- pages/members/index.js
+++ pages/members/index.js
@@ -1,8 +1,31 @@
+import Member from '../../models/memberModel'
+import styles from '../../styles/Home.module.css'
+import dbConnect from '../../db/connect'
+import Meta from '../../layouts/Meta'
 
-const MembersPage = () => {
+const MembersPage = ({members}) => {
   return (
-    <div>Members Page</div>
+    <div className={styles.container}>
+        <Meta description="Membres de l\'association Yigli Family" />
+        <p>{members[0].lastName}</p>
+
+    </div>
   )
 }
 
 export default MembersPage
+
+export const getServerSideProps = async () => {
+    await dbConnect()
+
+    const memberResult = await Member.find()
+    const members = memberResult.map(doc => {
+        const member = doc.toObject()
+        member._id = member._id.toString()
+        return member
+    })
+
+    return {
+        props: { members: JSON.parse(JSON.stringify(members)) }
+    }
+}
