b851ffebe0801fc6166301f3e6075407480583ef
Dave Mbassi <dcmbassi@gmail.com>
2022-07-19 17:49:26 +0100

    fix: Retrieve member by id for details page

--- pages/members/[memberId]/index.js
+++ pages/members/[memberId]/index.js
@@ -1,8 +1,28 @@
+import dbConnect from "../../../db/connect"
+import Member from '../../../models/memberModel'
 
-const MemberDetailsPage = () => {
-  return (
-    <div>Member Details Page</div>
-  )
+const MemberDetailsPage = ({ member }) => {
+    console.log({member})
+    return (
+        <>
+            <div>Member Details Page</div>
+            test
+            <p>{member.firstName}</p>
+        </>
+    )
 }
 
 export default MemberDetailsPage
+
+export const getServerSideProps = async (context) => {
+    const { memberId } = context.params
+
+    await dbConnect()
+
+    const member = await Member.findById(memberId).lean()
+
+    return {
+        props: { member: JSON.parse(JSON.stringify(member)) }
+    }
+
+}
