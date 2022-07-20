32726d602b800752a17c451276d95d3660d33a97
Dave Mbassi <dcmbassi@gmail.com>
2022-07-20 20:03:23 +0100

    fix: Clean up home page

--- pages/index.js
+++ pages/index.js
@@ -25,8 +25,6 @@ export default function Home({ meetings, totalContributions}) {
                     <MeetingsTable meetings={pastMeetings} />
                 </div>
             </main>
-
-
         </div>
     )
 }
