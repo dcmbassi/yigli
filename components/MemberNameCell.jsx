40d209c393f993db7894bb9e73963e78f8ec4749
Dave Mbassi <dcmbassi@gmail.com>
2022-07-24 09:52:15 +0100

    fix: Split icon and text into two typographies

--- components/MemberNameCell.jsx
+++ components/MemberNameCell.jsx
@@ -2,32 +2,44 @@ import Stack from "@mui/material/Stack"
 import Typography from "@mui/material/Typography";
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 
-const MemberNameCell = ({memberName, memberAddress}) => {
+const MemberNameCell = ({ memberName, memberAddress }) => {
     return (
         <Stack
             sx={{ width: '100%' }}
-            spacing={1}
+            spacing={2}
             justifyContent={{ sm: 'flex-start', md: 'space-between' }}
             alignItems={{ sm: 'flex-start', md: 'center' }}
             direction={{ xs: 'column', sm: 'column', md: 'row' }}
         >
             <Typography
-                
-                sx={{ 
-                    typography: { xs: 'h4', sm: 'h3', md: 'h2' }, 
-                    fontSize: {xs: '2.125rem', sm: '2.75rem', md: '3rem'}
+
+                sx={{
+                    typography: { xs: 'h4', sm: 'h3', md: 'h2' },
+                    fontSize: { xs: '2.125rem', sm: '2.75rem', md: '3rem' }
                 }}
-                textAlign={{ xs: 'center', sm: 'left'}}
+                textAlign={{ xs: 'center', sm: 'left' }}
                 gutterBottom
             >
                 {memberName}
             </Typography>
-            <Typography
+            <Stack
                 color='text.secondary'
-                textAlign={{ xs: 'center', sm: 'left'}}
+                direction='row'
+                justifyContent={{ xs: 'center', sm: 'flex-start' }}
+                alignItems='center'
             >
-                <LocationOnIcon mr={1} /> {memberAddress}
-            </Typography>
+                <Typography
+                // textAlign={{ xs: 'center', sm: 'left' }}
+                >
+                    <LocationOnIcon mr={1} />
+                </Typography>
+                <Typography
+                // textAlign={{ xs: 'center', sm: 'left' }}
+                >
+                    {memberAddress}22
+                </Typography>
+
+            </Stack>
         </Stack>
     )
 }
