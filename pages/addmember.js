4a6bdf40e59a34231850ec1330f200844bf890ee
Dave Mbassi <dcmbassi@gmail.com>
2022-08-16 17:28:28 +0100

    fix: Replace ModularForm for adding members with a custom AddMemberForm

--- pages/addmember.js
+++ pages/addmember.js
@@ -1,12 +1,15 @@
 import ModularForm from '../components/ModularForm'
 import styles from '../styles/Home.module.css'
 import { memberFields } from "../src/utils/formData";
+import Container from '@mui/material/Container';
+import AddMemberForm from '../components/forms/AddMemberForm';
 
 const addmember = () => {
     return (
-        <main className={styles.main}>
-            <ModularForm data={memberFields} />
-        </main>
+        <Container>
+            {/* <ModularForm data={memberFields} /> */}
+            <AddMemberForm />
+        </Container>
     )
 }
 
