7e3f4d87821a5e0042583624f8995b34a6c811e5
Dave Mbassi <dcmbassi@gmail.com>
2022-08-17 20:43:00 +0100

    fix: Add relevant fields to EditMemberForm

--- components/forms/EditMemberForm.jsx
+++ components/forms/EditMemberForm.jsx
@@ -4,6 +4,8 @@ import Button from '@mui/material/Button'
 import FormControl from '@mui/material/FormControl'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import FormLabel from '@mui/material/FormLabel'
+import InputLabel from '@mui/material/InputLabel'
+import NativeSelect from '@mui/material/NativeSelect'
 import Radio from '@mui/material/Radio'
 import RadioGroup from '@mui/material/RadioGroup'
 import Stack from "@mui/material/Stack"
@@ -32,27 +34,119 @@ const EditMemberForm = () => {
                         />
                         <TextField
                             type='text'
-                            name='firstName'
-                            label='Prénom'
+                            name='lastName'
+                            label='Nom'
+                            size='small'
+                            fullWidth
+                        />
+                        <TextField
+                            type='email'
+                            name='email'
+                            label='Email'
+                            size='small'
+                            fullWidth
+                        />
+                        <TextField
+                            type='text'
+                            name='address'
+                            label='Adresse'
                             size='small'
                             fullWidth
                         />
                     </Stack>
                     <Stack direction='column' spacing={2}>
-                        <TextField
-                            type='text'
-                            name='firstName'
-                            label='Prénom'
-                            size='small'
-                            fullWidth
-                        />
-                        <TextField
-                            type='text'
-                            name='firstName'
-                            label='Prénom'
-                            size='small'
-                            fullWidth
-                        />
+                        <FormControl>
+                            <InputLabel variant='standard' htmlFor='generation'>
+                                Génération
+                            </InputLabel>
+                            <NativeSelect
+                                // onChange={handleInputChange}
+                                // value={formValues[item.name]}
+                                inputProps={{
+                                    name: 'generation',
+                                    id: 'generation'
+                                }}
+                            >
+                                <option>
+                                    Gen 1
+                                </option>
+                                <option>
+                                    Gen 2
+                                </option>
+                                <option>
+                                    Gen 3
+                                </option>
+                            </NativeSelect>
+                        </FormControl>
+                        <FormControl>
+                            <InputLabel variant='standard' htmlFor='parents'>
+                                Parents
+                            </InputLabel>
+                            <NativeSelect
+                                // onChange={handleInputChange}
+                                // value={formValues[item.name]}
+                                inputProps={{
+                                    name: 'parents',
+                                    id: 'parents'
+                                }}
+                            >
+                                <option>
+                                    Parent 1
+                                </option>
+                                <option>
+                                    Parent 2
+                                </option>
+                                <option>
+                                    Parent 3
+                                </option>
+                            </NativeSelect>
+                        </FormControl>
+                        <FormControl>
+                            <InputLabel variant='standard' htmlFor='spouse'>
+                                Conjoint(e)
+                            </InputLabel>
+                            <NativeSelect
+                                // onChange={handleInputChange}
+                                // value={formValues[item.name]}
+                                inputProps={{
+                                    name: 'spouse',
+                                    id: 'spouse'
+                                }}
+                            >
+                                <option>
+                                    Spouse 1
+                                </option>
+                                <option>
+                                    Spouse 2
+                                </option>
+                                <option>
+                                    Spouse 3
+                                </option>
+                            </NativeSelect>
+                        </FormControl>
+                        <FormControl>
+                            <InputLabel variant='standard' htmlFor='children'>
+                                Enfants
+                            </InputLabel>
+                            <NativeSelect
+                                // onChange={handleInputChange}
+                                // value={formValues[item.name]}
+                                inputProps={{
+                                    name: 'children',
+                                    id: 'children'
+                                }}
+                            >
+                                <option>
+                                    Child 1
+                                </option>
+                                <option>
+                                    Child 2
+                                </option>
+                                <option>
+                                    Child 3
+                                </option>
+                            </NativeSelect>
+                        </FormControl>
                     </Stack>
                 </Stack>
                 <Button
