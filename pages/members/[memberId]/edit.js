6af382871ebb50ee279c8d56aed65a39768720de
Dave Mbassi <dcmbassi@gmail.com>
2022-08-17 21:24:58 +0100

    fix: Fetch member in EditMemberPage and pass it to the EditMemberForm

--- pages/members/[memberId]/edit.js
+++ pages/members/[memberId]/edit.js
@@ -1,12 +1,30 @@
 import { Container } from "@mui/material"
 import EditMemberForm from "../../../components/forms/EditMemberForm"
 
-const EditMemberPage = () => {
+import dbConnect from "../../../db/connect"
+import Member from '../../../models/memberModel'
+
+const EditMemberPage = ({member}) => {
     return (
         <Container>
-            <EditMemberForm />
+            <EditMemberForm member={member} />
         </Container>
     )
 }
 
 export default EditMemberPage
+
+export const getServerSideProps = async (context) => {
+    const { memberId } = context.params
+
+    await dbConnect()
+
+    const member = await Member.findById(memberId).lean()
+    delete member.password
+    delete member.createdAt
+    delete member.updatedAt
+
+    return {
+        props: { member: JSON.parse(JSON.stringify(member)) }
+    }
+}
