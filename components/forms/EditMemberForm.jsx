cad28cc549509384de8b4913986f9d6fec3de55e
Dave Mbassi <dcmbassi@gmail.com>
2022-08-18 18:11:31 +0100

    fix: Remove NativeSelect in favour of MUI Select component in EditMemberForm

--- components/forms/EditMemberForm.jsx
+++ components/forms/EditMemberForm.jsx
@@ -1,27 +1,23 @@
+import { useReducer } from "react"
+
 import Alert from "@mui/material/Alert"
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import FormControl from '@mui/material/FormControl'
-import FormControlLabel from '@mui/material/FormControlLabel'
-import FormLabel from '@mui/material/FormLabel'
 import Grid from '@mui/material/Grid'
 import InputLabel from '@mui/material/InputLabel'
-import NativeSelect from '@mui/material/NativeSelect'
-import Radio from '@mui/material/Radio'
-import RadioGroup from '@mui/material/RadioGroup'
+import Select from '@mui/material/Select'
 import Stack from "@mui/material/Stack"
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
-import { useReducer } from "react"
+
 import { editFormReducer } from "../../src/reducers/editFormReducer"
 
 /*
     TO DO
-    2. Initialise component state to member prop attributes
     3. Handle input changes
     4. Validate inputs
     5. Submit the form via a helper put function
-    6. Fix label presence issues in TextFields
 */
 
 const EditMemberForm = ({ member }) => {
@@ -35,6 +31,7 @@ const EditMemberForm = ({ member }) => {
                 </Typography>
                 <Grid
                     container
+                    minWidth={{ md: '600px' }}
                     spacing={4}
                     mb={4}
                 >
@@ -44,7 +41,7 @@ const EditMemberForm = ({ member }) => {
                                 type='text'
                                 name='firstName'
                                 label='Prénom'
-                                InputLabelProps={{ shrink:  true}}
+                                InputLabelProps={{ shrink: true }}
                                 value={state.firstName}
                                 size='small'
                                 fullWidth
@@ -53,7 +50,7 @@ const EditMemberForm = ({ member }) => {
                                 type='text'
                                 name='lastName'
                                 label='Nom'
-                                InputLabelProps={{ shrink:  true}}
+                                InputLabelProps={{ shrink: true }}
                                 value={state.lastName}
                                 size='small'
                                 fullWidth
@@ -62,7 +59,7 @@ const EditMemberForm = ({ member }) => {
                                 type='email'
                                 name='email'
                                 label='Email'
-                                InputLabelProps={{ shrink:  true}}
+                                InputLabelProps={{ shrink: true }}
                                 value={state.email}
                                 size='small'
                                 fullWidth
@@ -71,7 +68,7 @@ const EditMemberForm = ({ member }) => {
                                 type='text'
                                 name='address'
                                 label='Adresse'
-                                InputLabelProps={{ shrink:  true}}
+                                InputLabelProps={{ shrink: true }}
                                 value={state.address}
                                 size='small'
                                 fullWidth
@@ -79,117 +76,76 @@ const EditMemberForm = ({ member }) => {
                         </Stack>
                     </Grid>
                     <Grid item xs={12} md={6}>
-                        <Stack direction='column' spacing={2}>
-                        <FormControl size='small'>
-                            <InputLabel variant='standard' htmlFor='generation'>
-                                Génération
-                            </InputLabel>
-                            <NativeSelect
-                                // onChange={handleInputChange}
-                                // value={formValues[item.name]}
-                                size="small"
-                                multiple
-                                variant="outlined"
-                                inputProps={{
-                                    name: 'generation',
-                                    id: 'generation'
-                                }}
-                            >
-                                <option>
-                                    Gen 1
-                                </option>
-                                <option>
-                                    Gen 2
-                                </option>
-                                <option>
-                                    Gen 3
-                                </option>
-                            </NativeSelect>
-                        </FormControl>
-                        <FormControl size='small'>
-                            <InputLabel variant='standard' htmlFor='parents'>
-                                Parents
-                            </InputLabel>
-                            <NativeSelect
-                                // onChange={handleInputChange}
-                                // value={formValues[item.name]}
-                                inputProps={{
-                                    name: 'parents',
-                                    id: 'parents'
-                                }}
-                            >
-                                <option>
-                                    Parent 1
-                                </option>
-                                <option>
-                                    Parent 2
-                                </option>
-                                <option>
-                                    Parent 3
-                                </option>
-                            </NativeSelect>
-                        </FormControl>
-                        <FormControl size='small'>
-                            <InputLabel variant='standard' htmlFor='spouse'>
-                                Conjoint(e)
-                            </InputLabel>
-                            <NativeSelect
-                                // onChange={handleInputChange}
-                                // value={formValues[item.name]}
-                                inputProps={{
-                                    name: 'spouse',
-                                    id: 'spouse'
-                                }}
-                            >
-                                <option>
-                                    Spouse 1
-                                </option>
-                                <option>
-                                    Spouse 2
-                                </option>
-                                <option>
-                                    Spouse 3
-                                </option>
-                            </NativeSelect>
-                        </FormControl>
-                        <FormControl size='small'>
-                            <InputLabel variant='standard' htmlFor='children'>
-                                Enfants
-                            </InputLabel>
-                            <NativeSelect
-                                // onChange={handleInputChange}
-                                // value={formValues[item.name]}
-                                inputProps={{
-                                    name: 'children',
-                                    id: 'children'
-                                }}
-                            >
-                                <option>
-                                    Child 1
-                                </option>
-                                <option>
-                                    Child 2
-                                </option>
-                                <option>
-                                    Child 3
-                                </option>
-                            </NativeSelect>
-                        </FormControl>
+                        <Stack direction='column' spacing={3}>
+                            <FormControl size='small'>
+                                <InputLabel id='generation-label' >
+                                    Génération
+                                </InputLabel>
+                                <Select
+                                    labelId='generation-label'
+                                    id='generation'
+                                    label='Génération'
+                                    name='generation'
+                                >
+                                    <option>LFDF</option>
+                                    <option>DKKS</option>
+                                </Select>
+                            </FormControl>
+                            <FormControl size='small'>
+                                <InputLabel id='parents-label' >
+                                    Parents
+                                </InputLabel>
+                                <Select
+                                    labelId='parents-label'
+                                    id='parents'
+                                    label='Parents'
+                                    name='parents'
+                                >
+                                    <option>LFDF</option>
+                                    <option>DKKS</option>
+                                </Select>
+                            </FormControl>
+                            <FormControl size='small'>
+                                <InputLabel id='spouse-label' >
+                                    Conjoint(e)
+                                </InputLabel>
+                                <Select
+                                    labelId='spouse-label'
+                                    id='spouse'
+                                    label='Conjoint(e)'
+                                    name='spouse'
+                                >
+                                    <option>LFDF</option>
+                                    <option>DKKS</option>
+                                </Select>
+                            </FormControl>
+                            <FormControl size='small'>
+                                <InputLabel id='children-label' >
+                                    Children
+                                </InputLabel>
+                                <Select
+                                    labelId='children-label'
+                                    id='children'
+                                    label='Children'
+                                    name='children'
+                                >
+                                    <option>LFDF</option>
+                                    <option>DKKS</option>
+                                </Select>
+                            </FormControl>
                         </Stack>
                     </Grid>
                 </Grid>
-                <Button
-                    variant='contained'
-                    type='submit'
-                    fullWidth
-                >
-                    Enregistrer Changements
-                </Button>
+                <Box maxWidth={400} mx='auto' mt={{xs: 2, md: 8}}>
+                    <Button
+                        variant='contained'
+                        type='submit'
+                        fullWidth
+                    >
+                        Enregistrer Changements
+                    </Button>
+                </Box>
             </Box>
-
-
-
-
         </Box>
     )
 }
