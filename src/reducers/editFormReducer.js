5d8b292fc05c4bda70b10f08c18ea1c66cd14692
Dave Mbassi <dcmbassi@gmail.com>
2022-08-18 21:26:19 +0100

    fix: Scaffold editFormReducer function

---
+++ src/reducers/editFormReducer.js
@@ -0,0 +1,16 @@
+
+export const editFormReducer = (state, action) => {
+    switch (action.type) {
+        case 'CHANGE_INPUT':
+            
+            break;
+        case 'ADD_TO_ARRAY':
+
+            break;
+        case 'REMOVE_FROM_ARRAY':
+
+            break;
+        default:
+            return state
+    }
+}
