9010aa544d2b10827493bd726de6bcbfb29c4026
Dave Mbassi <dcmbassi@gmail.com>
2022-08-26 22:52:26 +0100

    fix: Replace modular form with contribution form in AddContributionPage

--- pages/addcontribution.js
+++ pages/addcontribution.js
@@ -1,15 +1,19 @@
+import Container from '@mui/material/Container'
+
 import ModularForm from '../components/ModularForm'
 import Member from '../models/memberModel'
 import styles from '../styles/Home.module.css'
 import dbConnect from '../db/connect'
 import { contributionFields } from "../src/utils/formData";
+import AddContributionForm from '../components/forms/AddContributionForm'
 
 
 const addcontribution = ({members}) => {
     return (
-        <main className={styles.main}>
-            <ModularForm data={contributionFields} members={members}/>
-        </main>
+        <Container>
+            {/* <ModularForm data={contributionFields} members={members}/> */}
+            <AddContributionForm />
+        </Container>
     )
 }
 
