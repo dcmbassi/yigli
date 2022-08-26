18747ac84be68bc797d6abc4b9a82d1fccd0f8f5
Dave Mbassi <dcmbassi@gmail.com>
2022-08-26 12:37:51 +0100

    fix: Turn each list item to a link to that member's detail page

--- components/MemberSublist.jsx
+++ components/MemberSublist.jsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import Avatar from '@mui/material/Avatar'
 import Box from '@mui/material/Box'
 import List from '@mui/material/List'
@@ -8,14 +10,19 @@ import ListItemText from '@mui/material/ListItemText'
 const MemberSublist = ({ members, title }) => {
     return (
         <Box mb={2} >
-            <List dense={true} subheader={title} sx={{display: {xs: 'flex', sm: 'block'}}}>
+            <List dense={true} subheader={title} sx={{ display: { xs: 'flex', sm: 'block' } }}>
                 {members.map(member => (
-                    <ListItem key={member._id} >
-                        <ListItemAvatar >
-                            <Avatar alt={member.firstName} src='/img/sample.jpg' />
-                        </ListItemAvatar>
-                        <ListItemText primary={member.firstName} />
-                    </ListItem>
+                    <Link key={member._id} href={`/members/${member._id}`}>
+                        <ListItem 
+                        button={true} 
+                        sx={{boxShadow: 1, borderRadius: '8px', marginBottom: '4px'}}
+                        >
+                            <ListItemAvatar >
+                                <Avatar alt={member.firstName} src='/img/sample.jpg' />
+                            </ListItemAvatar>
+                            <ListItemText primary={member.firstName} />
+                        </ListItem>
+                    </Link>
                 ))}
             </List>
         </Box>
