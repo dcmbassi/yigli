cc90f32e8d3852f11d093891c6c5e639abb61d1b
Dave Mbassi <dcmbassi@gmail.com>
2022-08-29 08:36:16 +0100

    fix: Spread state return values in formReducer

--- src/reducers/formReducer.js
+++ src/reducers/formReducer.js
@@ -4,12 +4,12 @@ export const formReducer = (state, action) => {
     switch (action.type) {
         case ACTIONS.CHANGE_INPUT:
             return {
-                state,
+                ...state,
                 [action.payload.name]: action.payload.value
             }
         case ACTIONS.CHANGE_ARRAY:
             return {
-                state,
+                ...state,
                 [action.payload.name]: [...action.payload.trueValue]
             }
         case ACTIONS.RESET:
