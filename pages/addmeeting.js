306c24107d6cceba72dce3b8f62f3d7a20bbd5c5
Dave Mbassi <dcmbassi@gmail.com>
2022-08-30 15:34:19 +0100

    fix: Fetch members in AddMeetingPage, pass them to form

--- pages/addmeeting.js
+++ pages/addmeeting.js
@@ -3,14 +3,29 @@ import Container from '@mui/material/Container'
 import ModularForm from '../components/ModularForm'
 import styles from '../styles/Home.module.css'
 import { meetingFields } from '../src/utils/formData'
+import Member from '../models/memberModel'
 import AddMeetingForm from '../components/forms/AddMeetingForm'
+import dbConnect from '../db/connect'
 
-const addmeeting = () => {
-  return (
-    <Container>
-        <AddMeetingForm />
-    </Container>
-  )
+const addmeeting = ({members}) => {
+    console.log(members[10])
+    return (
+        <Container>
+            <AddMeetingForm members={members} />
+        </Container>
+    )
+}
+
+export const getServerSideProps = async () => {
+    await dbConnect()
+
+    const members = await Member.find().lean()
+
+    return {
+        props: {
+            members: JSON.parse(JSON.stringify(members)),
+        }
+    }
 }
 
 export default addmeeting
