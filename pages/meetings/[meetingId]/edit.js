7edae59bd0a854e06847988ab0abf5c1138061cb
Dave Mbassi <dcmbassi@gmail.com>
2022-09-02 17:16:19 +0100

    fix: Implement logic for EdiMeetingPage

--- pages/meetings/[meetingId]/edit.js
+++ pages/meetings/[meetingId]/edit.js
@@ -1,8 +1,31 @@
+import Container from '@mui/material/Container'
+import EditMeetingForm from '../../../components/forms/EditMeetingForm'
 
-const EditMeetingPage = () => {
-  return (
-    <div>Edit Meeting Page</div>
-  )
+import dbConnect from '../../../db/connect'
+import Meeting from '../../../models/meetingModel'
+
+
+const EditMeetingPage = ({meeting}) => {
+    console.log({meeting})
+    return (
+        <Container>
+            <EditMeetingForm />
+        </Container>
+    )
 }
 
 export default EditMeetingPage
+
+export const getServerSideProps = async (context) => {
+    const { meetingId } = context.params
+    await dbConnect()
+
+    const meeting = await Meeting.findById(meetingId).lean()
+
+    return {
+        props: {
+            meeting: JSON.parse(JSON.stringify(meeting)),
+        }
+    }
+
+}
