4f45daa95e18917edd714960629018b1cbc7c10f
Dave Mbassi <dcmbassi@gmail.com>
2022-09-02 17:18:46 +0100

    fix: Implement logic for MeetingsPage

--- pages/meetings/index.js
+++ pages/meetings/index.js
@@ -1,8 +1,33 @@
+import Container from '@mui/material/Container'
 
-const MeetingsPage = () => {
-  return (
-    <div>Meetings Page</div>
-  )
+import Meeting from '../../models/meetingModel'
+import dbConnect from '../../db/connect'
+import MeetingListing from '../../components/MeetingListing'
+
+
+const MeetingsPage = ({ meetings }) => {
+    const upcoming = meetings.filter(meeting => meeting.upcoming)
+    const past = meetings.filter(meeting => !meeting.upcoming)
+    return (
+        <Container>
+            <MeetingListing 
+                upcoming={upcoming}
+                past={past}
+            />
+        </Container>
+    )
 }
 
 export default MeetingsPage
+
+export const getServerSideProps = async () => {
+    await dbConnect()
+
+    const meetings = await Meeting.find().lean()
+
+    return {
+        props: {
+            meetings: JSON.parse(JSON.stringify(meetings)),
+        }
+    }
+}
