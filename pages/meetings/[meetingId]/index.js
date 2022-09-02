10ac1ecb33d8820adb93c3ab829c5847e37bdda9
Dave Mbassi <dcmbassi@gmail.com>
2022-09-02 17:20:13 +0100

    fix: Implement backend logic for MeetingDetailsPage

--- pages/meetings/[meetingId]/index.js
+++ pages/meetings/[meetingId]/index.js
@@ -1,8 +1,26 @@
+import dbConnect from '../../../db/connect'
+import Meeting from '../../../models/meetingModel'
+// import Member from '../../../models/memberModel'
 
-const MeetingDetailsPage = () => {
-  return (
-    <div>Meeting Details Page</div>
-  )
+const MeetingDetailsPage = ({ meeting }) => {
+    console.log(meeting)
+    return (
+        <div>Meeting Details Page</div>
+    )
 }
 
 export default MeetingDetailsPage
+
+export const getServerSideProps = async (context) => {
+    const { meetingId } = context.params
+
+    await dbConnect()
+
+    const meeting = await Meeting.findById(meetingId).populate('hosts').lean()
+
+    return {
+        props: {
+            meeting: JSON.parse(JSON.stringify(meeting))
+        }
+    }
+}
