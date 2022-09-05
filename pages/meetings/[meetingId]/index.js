118c76ad94eda0586b7b203369393f7889342495
Dave Mbassi <dcmbassi@gmail.com>
2022-09-05 17:36:30 +0100

    fix: Use Promise.All to address model base error

--- pages/meetings/[meetingId]/index.js
+++ pages/meetings/[meetingId]/index.js
@@ -1,11 +1,31 @@
+import Link from 'next/link';
+
+import Container from "@mui/material/Container"
+import Button from "@mui/material/Button"
+import Box from "@mui/material/Box"
+import EditIcon from '@mui/icons-material/Edit'
+
 import dbConnect from '../../../db/connect'
 import Meeting from '../../../models/meetingModel'
-// import Member from '../../../models/memberModel'
+import Member from '../../../models/memberModel'
 
 const MeetingDetailsPage = ({ meeting }) => {
-    console.log(meeting)
+    const editUrl = `/meetings/${meeting._id}/edit`
     return (
-        <div>Meeting Details Page</div>
+        <Container>
+            <Box px={4} mt={2} display='flex' justifyContent='flex-end'>
+                <Link href={editUrl} passHref>
+                    <Button
+                        variant='outlined'
+                        size='small'
+                        startIcon={<EditIcon />}
+                    >
+                        Modifier
+                    </Button>
+                </Link>
+            </Box>
+
+        </Container>
     )
 }
 
@@ -15,8 +35,12 @@ export const getServerSideProps = async (context) => {
     const { meetingId } = context.params
 
     await dbConnect()
+    const [meeting] = await Promise.all([
+        Meeting.findById(meetingId).populate('hosts').lean(),
+        Member.find().lean()
+    ])
 
-    const meeting = await Meeting.findById(meetingId).populate('hosts').lean()
+    // const meeting = await Meeting.findById(meetingId).populate('hosts').lean()
 
     return {
         props: {
