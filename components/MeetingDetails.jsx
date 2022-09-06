8d5f2f3f10b1affd9b5e5b2351528272b097bbd3
Dave Mbassi <dcmbassi@gmail.com>
2022-09-06 17:11:43 +0100

    fix: Set up initial layout for meeting details

---
+++ components/MeetingDetails.jsx
@@ -0,0 +1,34 @@
+import Box from '@mui/material/Box'
+import Stack from "@mui/material/Stack"
+import Typography from "@mui/material/Typography"
+import { extractDate } from '../src/utils/helpers'
+
+const MeetingDetails = ({meeting}) => {
+  return (
+    <Box p={2} sx={{width: '100%'}}>
+        <Stack>
+            <Stack>
+                <Typography variant='h5' textAlign='center'>
+                    {extractDate(meeting.date)}
+                </Typography>
+                <Typography variant='h6' textAlign='center'>
+                    {meeting.location}
+                </Typography>
+                <Box>
+                    {meeting.hosts.map(host => (
+                        <Typography key={host._id}>
+                            {`${host.firstName} ${host.lastName}`}
+                        </Typography>
+                    ))}
+                </Box>
+            </Stack>
+
+            <Stack>
+
+            </Stack>
+        </Stack>
+    </Box>
+  )
+}
+
+export default MeetingDetails
