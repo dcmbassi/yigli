91de990d86e8466554ed54320b6fda378aca299d
Dave Mbassi <dcmbassi@gmail.com>
2022-09-13 10:23:39 +0100

    feat: Add helper function for pre-filling date inputs

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -9,17 +9,28 @@ const extractDate = (dateTimeString) => {
     return `${day}/${month}/${date.getFullYear()}`
 }
 
+const prefillDate = (dateTimeString) => {
+    const date = new Date(dateTimeString)
+
+    let day = date.getDate()
+    let month = date.getMonth() + 1
+    if (day < 10) day = `0${day}`
+    if (month < 10) month = `0${month}`
+
+    return `${date.getFullYear()}-${month}-${day}`
+}
+
 const formatCurrency = (amount, currency = 'FCFA') => {
     return `${amount} ${currency}`
 }
 
 const addProperty = (propName, target, source) => {
-    console.log('adding...');
     target[propName] = {...source}
 }
 
 export {
     extractDate,
+    prefillDate,
     formatCurrency,
     addProperty
 }
