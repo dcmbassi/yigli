2a7c3269bd6ecf884c95aaa04c1ef7b6a4c089d8
Dave Mbassi <dcmbassi@gmail.com>
2022-09-13 14:34:31 +0100

    fix: Fetch members on the meeting details page

--- pages/meetings/[meetingId]/index.js
+++ pages/meetings/[meetingId]/index.js
@@ -10,7 +10,7 @@ import Meeting from '../../../models/meetingModel'
 import Member from '../../../models/memberModel'
 import MeetingDetails from '../../../components/MeetingDetails';
 
-const MeetingDetailsPage = ({ meeting }) => {
+const MeetingDetailsPage = ({ meeting, members }) => {
     const editUrl = `/meetings/${meeting._id}/edit`
     return (
         <Container>
@@ -25,7 +25,7 @@ const MeetingDetailsPage = ({ meeting }) => {
                     </Button>
                 </Link>
             </Box>
-            <MeetingDetails meeting={meeting} />
+            <MeetingDetails meeting={meeting} members={members} />
         </Container>
     )
 }
@@ -36,16 +36,17 @@ export const getServerSideProps = async (context) => {
     const { meetingId } = context.params
 
     await dbConnect()
-    const [meeting] = await Promise.all([
+    const [meeting, members] = await Promise.all([
         Meeting.findById(meetingId).populate('hosts').lean(),
-        Member.find().lean()
+        Member.find().select({password: 0}).lean()
     ])
 
     // const meeting = await Meeting.findById(meetingId).populate('hosts').lean()
 
     return {
         props: {
-            meeting: JSON.parse(JSON.stringify(meeting))
+            meeting: JSON.parse(JSON.stringify(meeting)),
+            members: JSON.parse(JSON.stringify(members)),
         }
     }
 }
