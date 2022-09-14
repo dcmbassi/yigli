4f38c185b6b8ee1dfb85f95fbd416319ac9549d1
Dave Mbassi <dcmbassi@gmail.com>
2022-09-14 21:35:45 +0100

    fix: Place add item to the right

--- components/MeetingDetails.jsx
+++ components/MeetingDetails.jsx
@@ -10,7 +10,7 @@ import { extractDate } from '../src/utils/helpers'
 import MinuteList from './MinuteList'
 import MinuteItemDialog from './MinuteItemDialog'
 
-const MeetingDetails = ({meeting, members, agendaItems}) => {
+const MeetingDetails = ({ meeting, members, agendaItems }) => {
     const [open, setOpen] = useState(false)
 
     const handleOpen = () => setOpen(true)
@@ -18,54 +18,57 @@ const MeetingDetails = ({meeting, members, agendaItems}) => {
     const handleClose = () => setOpen(false)
 
     return (
-    <Box p={2} sx={{width: '100%'}}>
-        <Stack>
+        <Box p={2} sx={{ width: '100%' }}>
             <Stack>
-                <Typography variant='h5' textAlign='center'>
-                    {extractDate(meeting.date)}
-                </Typography>
-                <Typography variant='h6' textAlign='center'>
-                    {meeting.location}
-                </Typography>
-                <Box>{meeting.hosts.length < 2 ? 'Hôte: ': 'Hôtes: '}
-                    {meeting.hosts.map(host => (
-                        <Typography key={host._id}>
-                            {`${host.firstName} ${host.lastName}`}
-                        </Typography>
-                    ))}
-                </Box>
-            </Stack>
-
-            <Stack py={2} spacing={2}>
-                <Box display='flex' justifyContent='space-between'>
-                    <Typography>
-                        Retards: <span>5</span>
+                <Stack>
+                    <Typography variant='h5' textAlign='center'>
+                        {extractDate(meeting.date)}
                     </Typography>
-                    <Typography>
-                        Absences: <span>3</span>
-                    </Typography>
-                </Box>
-                <Box>
                     <Typography variant='h6' textAlign='center'>
-                        Procès-verbal
+                        {meeting.location}
                     </Typography>
-                    <Box>
-                        <Button
-                            variant='outlined'
-                            onClick={handleOpen}
-                            size='small'
-                            startIcon={<PlaylistAddIcon />}
-                        >
-                            Ajouter Objet
-                        </Button>
-                        <MinuteList items={agendaItems}/>
+                    <Box>{meeting.hosts.length < 2 ? 'Hôte: ' : 'Hôtes: '}
+                        {meeting.hosts.map(host => (
+                            <Typography key={host._id}>
+                                {`${host.firstName} ${host.lastName}`}
+                            </Typography>
+                        ))}
                     </Box>
-                </Box>
+                </Stack>
+
+                <Stack py={2} spacing={2}>
+                    <Box display='flex' justifyContent='space-between'>
+                        <Typography>
+                            Retards: <span>5</span>
+                        </Typography>
+                        <Typography>
+                            Absences: <span>3</span>
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <Typography variant='h6' textAlign='center'>
+                            Procès-verbal
+                        </Typography>
+                        <Box>
+                            <Box display='flex' justifyContent='flex-end'>
+                                <Button
+                                    variant='outlined'
+                                    onClick={handleOpen}
+                                    size='small'
+                                    startIcon={<PlaylistAddIcon />}
+                                >
+                                    Ajouter Objet
+                                </Button>
+
+                            </Box>
+                            <MinuteList items={agendaItems} />
+                        </Box>
+                    </Box>
+                </Stack>
             </Stack>
-        </Stack>
-        <MinuteItemDialog open={open} handleClose={handleClose} meetingId={meeting._id} members={members} />
-    </Box>
-  )
+            <MinuteItemDialog open={open} handleClose={handleClose} meetingId={meeting._id} members={members} />
+        </Box>
+    )
 }
 
 export default MeetingDetails
