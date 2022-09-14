aa19c2acb51644f9896f6de76f8766eb7d788f1e
Dave Mbassi <dcmbassi@gmail.com>
2022-09-14 21:37:45 +0100

    fix: Add data to MinuteListItem card

--- components/MinuteListItem.jsx
+++ components/MinuteListItem.jsx
@@ -1,20 +1,38 @@
+import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
-import CardActionArea from '@mui/material/CardActionArea'
 import CardContent from '@mui/material/CardContent'
+import Stack from '@mui/material/Stack'
 import Typography from '@mui/material/Typography'
 
-const MinuteListItem = ({item}) => {
-  return (
-    <Card>
-        <CardActionArea>
+const MinuteListItem = ({ item }) => {
+    return (
+        <Card>
             <CardContent>
-                <Typography>
-                    {item.title}
-                </Typography>
+                <Stack direction='row' spacing={3}>
+                    <Box>
+                        <Typography sx={{fontWeight: 'bold'}}>
+                            {item.title}
+                        </Typography>
+                        <Typography variant='caption' color='secondary'>
+                            {item.owner.firstName}
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <Typography>
+                            {item.description}
+                        </Typography>
+                    </Box>
+                    {item.resolution && (
+                        <Box>
+                            <Typography>
+                                {item.resolution}
+                            </Typography>
+                        </Box>
+                    )}
+                </Stack>
             </CardContent>
-        </CardActionArea>
-    </Card>
-  )
+        </Card>
+    )
 }
 
 export default MinuteListItem
