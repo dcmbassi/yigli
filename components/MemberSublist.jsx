fd8c8ecd65180fc052beabd4b8367136578fb26d
Dave Mbassi <dcmbassi@gmail.com>
2022-09-23 14:51:52 +0100

    fix: Adjust styling of member sublist

--- components/MemberSublist.jsx
+++ components/MemberSublist.jsx
@@ -10,12 +10,12 @@ import ListItemText from '@mui/material/ListItemText'
 const MemberSublist = ({ members, title }) => {
     return (
         <Box mb={2} >
-            <List dense={true} subheader={title} sx={{ display: { xs: 'flex', sm: 'block' } }}>
+            <List dense={true} subheader={title} sx={{ display: {sm: 'block' } }}>
                 {members.map(member => (
                     <Link key={member._id} href={`/members/${member._id}`}>
                         <ListItem
                             button={true}
-                            sx={{ boxShadow: 1, borderRadius: '8px', marginBottom: '4px' }}
+                            sx={{ boxShadow: 1, borderRadius: '8px', marginInline: 'auto', marginBottom: '8px', maxWidth: {xs: '80%', sm: '100%'} }}
                         >
                             <ListItemAvatar >
                                 <Avatar alt={member.firstName} src='/img/sample.jpg' />
