b68ebefadb9e80c063ee67cdd0c812b9ce75b6b1
Dave Mbassi <dcmbassi@gmail.com>
2022-09-23 17:42:34 +0100

    fix: Escape single quote in description string

--- pages/addgeneration.js
+++ pages/addgeneration.js
@@ -3,9 +3,10 @@ import AddGenerationForm from '../components/forms/AddGenerationForm';
 import Meta from '../layouts/Meta';
 
 const AddGenerationPage = () => {
+    const desc = 'La famille s\'agrandit. Une nouvelle génération arrive.'
     return (
         <Container>
-            <Meta title='Nouvelle génération' description='La famille s\agrandit. Une nouvelle génération arrive.' />
+            <Meta title='Nouvelle génération' description={desc} />
             <AddGenerationForm />
         </Container>
     )
