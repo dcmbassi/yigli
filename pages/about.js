e09074c0c31d342e609eaa4d71cde13011ba7b0e
Dave Mbassi <dcmbassi@gmail.com>
2022-09-27 11:25:05 +0100

    feat: Fetch populated positions in About page

--- pages/about.js
+++ pages/about.js
@@ -2,22 +2,37 @@ import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
+
+import Position from '../models/positionModel'
+import dbConnect from '../db/connect'
 import Meta from '../layouts/Meta'
 
-const AboutPage = () => {
-  return (
-    <Container maxWidth="sm">
-        <Meta title='A Propos' description='Héritage de Yigli' />
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Descendance de Jean-Baptiste Medoung, dit Yigli
-        </Typography>
-        <Button variant="contained"  href="/">
-          Retour à l&apos;accueil
-        </Button>
-      </Box>
-    </Container>
-  )
+const AboutPage = ({ positions }) => {
+    return (
+        <Container maxWidth="sm">
+            <Meta title='A Propos' description='Héritage de Yigli' />
+            <Box sx={{ my: 4 }}>
+                <Typography variant="h4" component="h1" gutterBottom>
+                    Descendance de Jean-Baptiste Medoung, dit Yigli
+                </Typography>
+                <Button variant="contained" href="/">
+                    Retour à l&apos;accueil
+                </Button>
+            </Box>
+        </Container>
+    )
+}
+
+export const getServerSideProps = async () => {
+    await dbConnect()
+
+    const positions = await Position.find().populate('appointee', 'firstName lastName').lean()
+
+    return {
+        props: {
+            positions: JSON.parse(JSON.stringify(positions))
+        }
+    }
 }
 
 export default AboutPage
